03091e5feadb3158b290ad302a5c075ddc35c2ef
Dimanoti <drystant@mail.ustc.edu.cn>
2026-08-16 20:03:40 +0800

    Change4

--- content/generated.ts
+++ content/generated.ts
@@ -1,8 +1,10 @@
 import article0 from "./Miller-Rabin.md";
-import article1 from "./prime.md";
+import article1 from "./composite.md";
+import article2 from "./prime.md";
 
 // This file is generated from content/*.md. Do not edit it manually.
 export const articleSources = {
   "Miller-Rabin": article0,
-  "prime": article1,
+  "composite": article1,
+  "prime": article2,
 } as const;
